6f8dd89d69027c429d0361235489936d00d710b8
adarsh kiratkudave <sangharsharanpise408@gmail.com>
2020-04-17 18:54:05 +0530

    dialog box is open and working on dropdown box

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,121 +1,121 @@
-  $(document).ready(function(){
-    $("#openNav").click(function(){
-      $(".w3-sidebar").toggle();
-    });
+$(document).ready(function(){
+  $("#openNav").click(function(){
+    $(".w3-sidebar").toggle();
   });
- 
-  $(document).ready(function(){
-    $("#note").click(function(){
-        $(".createNote").css("display","block")
-      $("#note").css("display","none");
-    });
+});
+
+$(document).ready(function(){
+  $("#note").click(function(){
+      $(".createNote").css("display","block")
+    $("#note").css("display","none");
   });
+});
 
 $(function() {
-  $("#title_error_message").hide();
-  $("#desc_error_message").hide();
+$("#title_error_message").hide();
+$("#desc_error_message").hide();
 
-  var error_title = false;
-  var error_desc = false;
+var error_title = false;
+var error_desc = false;
 
- 
-  $("#title").focusout(function() {
-     check_title();
-  });
-  $("#desc").focusout(function() {
-     check_desc();
-  });
- 
 
-  
-  function check_title() {
-    var pattern = /^[a-zA-Z ]*$/;
-    var title = $("#title").val()
-    if (pattern.test(title) && desc !== ''){
-         $("#title_error_message").hide();
-         $("#title").css("border-bottom","2px solid #34F458");
-      } else {
-         $("#title_error_message").html("Invalid title");
-         $("#title_error_message").show();
-         $("#title").css("border-bottom","2px solid #F90A0A");
-         error_title = true;
-      }
-   }
- 
-  function check_desc() {
-    var pattern = /^[a-zA-Z ]*$/;
-    var desc = $("#desc").val()
-    if (pattern.test(desc) && desc !== '') 
-    {
-      $("#desc_error_message").hide();
-      $("#title").css("border-bottom","2px solid #34F458");
+$("#title").focusout(function() {
+   check_title();
+});
+$("#desc").focusout(function() {
+   check_desc();
+});
+
+
+
+function check_title() {
+  var pattern = /^[a-zA-Z ]*$/;
+  var title = $("#title").val()
+  if (pattern.test(title) && desc !== ''){
+       $("#title_error_message").hide();
+       $("#title").css("border-bottom","2px solid #34F458");
     } else {
-      $("#desc_error_message").html("Invalid description");
-      $("#desc_error_message").show();
-      $("#desc").css("border-bottom","2px solid #F90A0A");
-      error_desc = true;
-   }
-  }
+       $("#title_error_message").html("Invalid title");
+       $("#title_error_message").show();
+       $("#title").css("border-bottom","2px solid #F90A0A");
+       error_title = true;
+    }
+ }
+
+function check_desc() {
+  var pattern = /^[a-zA-Z ]*$/;
+  var desc = $("#desc").val()
+  if (pattern.test(desc) && desc !== '') 
+  {
+    $("#desc_error_message").hide();
+    $("#title").css("border-bottom","2px solid #34F458");
+  } else {
+    $("#desc_error_message").html("Invalid description");
+    $("#desc_error_message").show();
+    $("#desc").css("border-bottom","2px solid #F90A0A");
+    error_desc = true;
+ }
+}
 
 $("#close").click(function () {
-  console.log("in button");
+console.log("in button");
+ 
+error_title = false;
+error_desc = false;
+
+check_title();
+check_desc();
+
+ const noteData={
+
+   title:$('#title').val(),
+   description:$('#desc').val(),
    
-  error_title = false;
-  error_desc = false;
+};
 
-  check_title();
-  check_desc();
 
-   const noteData={
+title=$('#title').val();
+description=$('#desc').val();
 
-     title:$('#title').val(),
-     description:$('#desc').val(),
-     
-  };
-
-  
-  title=$('#title').val();
-  description=$('#desc').val();
-
-  console.log("title=",title);
-  console.log("desc=",description);
+console.log("title=",title);
+console.log("desc=",description);
 console.log(noteData);
 
 
-  if ( error_title === false && error_desc === false)
-  {
-     var token= localStorage.getItem("token");
-     console.log("token =>",token);
-     console.log("notes data ",noteData);
-     
-     $.ajax({
-      url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes",
-      data:  JSON.stringify(noteData),  
-      type: "POST",
-      headers: {
-         'Authorization': localStorage.getItem('token')
-       },
-      contentType: "application/json;charset=utf-8",
-      success: function (result) {
-      console.log("note create result ==>", result);
+if ( error_title === false && error_desc === false)
+{
+   var token= localStorage.getItem("token");
+   console.log("token =>",token);
+   console.log("notes data ",noteData);
+   
+   $.ajax({
+    url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes",
+    data:  JSON.stringify(noteData),  
+    type: "POST",
+    headers: {
+       'Authorization': localStorage.getItem('token')
+     },
+    contentType: "application/json;charset=utf-8",
+    success: function (result) {
+    console.log("note create result ==>", result);
 
-       
-        console.log("id============>", result.id);
-        alert("note create Successfull");
-        resetForm();
-        $(location).attr('href',"/views/dashboard/dashboard.html");
-        },
-        error: function (errorMessage) {
-        console.log("Error", errorMessage);
-        alert("errorMessage==>",errorMessage.responseText);
-        }
-     })
-      return true;
-   } else {
-      alert("Please Fill Correctly");
-      return false;
-   }
-  });
+     
+      console.log("id============>", result.id);
+      alert("note create Successfull");
+      resetForm();
+      $(location).attr('href',"/views/dashboard/dashboard.html");
+      },
+      error: function (errorMessage) {
+      console.log("Error", errorMessage);
+      alert("errorMessage==>",errorMessage.responseText);
+      }
+   })
+    return true;
+ } else {
+    alert("Please Fill Correctly");
+    return false;
+ }
+});
 });
 function resetForm(){
 document.getElementById("title").value="";
@@ -123,43 +123,47 @@ document.getElementById("desc").value="";
 }
 
 /****************************** display note on dashboard ************************************************/
-  
+
 console.log("in display note");
-  
+
+// $(document).ready(function(){
+// $("#note").click(function(){
+//     $("#openNote").css("display","block")
+// });
+// });
+
+var token= localStorage.getItem("token");
+console.log("token =>",token);
+
+var notes=[];
 $(document).ready(function(){
-  $("#note").click(function(){
-      $("#openNote").css("display","block")
-  });
+
+  $.ajax({
+      
+      url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList",
+      type: "GET",
+      headers: { 'Authorization': localStorage.getItem('token') },
+      
+      success: function(result){
+      notes=result.data.data;
+      console.log("notes = ",notes);
+// setTimeout(() => {
+
+      var wrapper = document.getElementById("display");
+      var myHTML = '';
+    
+      for (var i = 0; i <notes.length; i++) {
+        //myHTML += '<div class="card" data-toggle="modal" data-target="#myModal"><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span>'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div><div class="icons" style="padding-right: 10px;"><i class="material-icons">add_alert</i><i class="material-icons">person_add</i><i class="fas fa-palette"></i><i class="far fa-image"></i><img src="/images/archieve.png" style="width: 20px; height: 18px;"> <i class="fas fa-ellipsis-v"></i> </div></div></div></div>';
+        myHTML +='<div class="card" data-toggle="modal" data-target="#myModal"><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span>'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div><div class="icon dropdown" style="padding-right: 10px; display: flex;"><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="material-icons">add_alert</i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="material-icons">person_add</i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="fas fa-palette"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="far fa-image"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><img src="/images/archieve.png" style="width: 20px; height: 18px;"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="fas fa-ellipsis-v"></i></div><ul class="dropdown-menu" role="menu" aria-labelledby="menu1"><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Delete note</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Change labels</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Add drawing</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Make a copy</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Show checkboxes</a></li></ul></div></div></div></div>';
+      }
+         wrapper.innerHTML = myHTML
+   // }, 1000);
+      
+   }});
 });
 
- var token= localStorage.getItem("token");
- console.log("token =>",token);
-
- var notes=[];
- $(document).ready(function(){
-  
-    $.ajax({
-        
-        url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList",
-        type: "GET",
-        headers: { 'Authorization': localStorage.getItem('token') },
-        
-        success: function(result){
-        notes=result.data.data;
-        console.log("notes = ",notes);
-setTimeout(() => {
-  
-        var wrapper = document.getElementById("display");
-        var myHTML = '';
-      
-        for (var i = 0; i <notes.length; i++) {
-          myHTML += '<div class="card" id="note"><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span>'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div><div class="icons" style="padding-right: 10px;"><i class="material-icons">add_alert</i><i class="material-icons">person_add</i><i class="fas fa-palette"></i><i class="far fa-image"></i><img src="/images/archieve.png" style="width: 20px; height: 18px;"> <i class="fas fa-ellipsis-v"></i> </div></div></div></div>';
-        }
-           wrapper.innerHTML = myHTML
-      }, 1000);
-        
-     }});
+$(document).ready(function () {
+  $(".fas fa-ellipsis-v").click(function () {
+    $(".dropdown").toggle();
   });
-
-
-  
+});
--- js/notes/note.js
+++ js/notes/note.js
@@ -1,51 +1,8 @@
-// console.log("in display note");
+/**************************************************************************/
+$(document).ready(function () {
+    $(".notification_icon .fa-bell").click(function () {
+      $(".dropdown").toggle();
+    });
+  });
   
-// $(document).ready(function(){
-//   $("#note").click(function(){
-//       alert("click");
-//       $("#openNote").css("display","block")
-//   });
-// });
-
-//  var token= localStorage.getItem("token");
-//  console.log("token =>",token);
-
-//  var notes=[];
-//  $(document).ready(function(){
-  
-//     $.ajax({
-        
-//         url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList",
-//         type: "GET",
-//         headers: { 'Authorization': localStorage.getItem('token') },
-        
-//         success: function(result){
-//         notes=result;
-//         console.log("notes = ",notes);
-
-//         var wrapper = document.getElementById("#display");
-//         var myHTML = '';
-      
-//         for (var i = 0; i <notes.length; i++) {
-//           myHTML += '<div class="card" id="note"><div class="card-body"><div class="info" style="color: black; "><input class="tit" id="tit" placeholder="title"><input id="desc" class="desc" style="margin-top: 20px;"placeholder="description"><div class="icons" style="padding-right: 10px;"><i class="material-icons">add_alert</i><i class="material-icons">person_add</i><i class="fas fa-palette"></i><i class="far fa-image"></i><img src="/images/archieve.png" style="width: 20px; height: 18px;"> <i class="fas fa-ellipsis-v"></i> </div></div></div></div>';
-//           document.getElementById("#tit").innerHTML = notes[i].title;
-//           document.getElementById("#desc").innerHTML = notes[i].description;
-//         }
-      
-//         wrapper.innerHTML = myHTML
-//      }});
-//   });
-
-  
-//         // var wrapper = document.getElementById("#display");
-//         // var myHTML = '';
-      
-//         // for (var i = 0; i <notes.length; i++) {
-//         //   myHTML += '<div class="card" id="note"><div class="card-body"><div class="info" style="color: black; "><input class="tit" id="tit" placeholder="title"><input id="desc" class="desc" style="margin-top: 20px;"placeholder="description"><div class="icons" style="padding-right: 10px;"><i class="material-icons">add_alert</i><i class="material-icons">person_add</i><i class="fas fa-palette"></i><i class="far fa-image"></i><img src="/images/archieve.png" style="width: 20px; height: 18px;"> <i class="fas fa-ellipsis-v"></i> </div></div></div></div>';
-//         //   document.getElementById("#tit").innerHTML = notes[i].title;
-//         //   document.getElementById("#desc").innerHTML = notes[i].description;
-//         // }
-      
-//         // wrapper.innerHTML = myHTML
-
-  
+ 
