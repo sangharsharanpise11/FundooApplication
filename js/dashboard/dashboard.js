8ee4dfccf519019c1a166a80893972398d8bfcd1
adarsh kiratkudave <sangharsharanpise408@gmail.com>
2020-04-20 18:01:36 +0530

    completed trash note and archive note

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -96,14 +96,14 @@ if ( error_title === false && error_desc === false)
        'Authorization': localStorage.getItem('token')
      },
     contentType: "application/json;charset=utf-8",
-    success: function (result) {
-    console.log("note create result ==>", result);
+    success: function (status) {
+    // console.log("note create result ==>", result);
 
-     
-      console.log("id============>", result.id);
-      alert("note create Successfull");
-      resetForm();
-      $(location).attr('href',"/views/dashboard/dashboard.html");
+    localStorage.setItem("NoteId ",status.collaborators.id);
+    console.log(localStorage.getItem('NoteId is :',NoteId));
+      console.log("id============>", result.data.data.id);
+      // resetForm();
+      // $(location).attr('href',"/views/dashboard/dashboard.html");
       },
       error: function (errorMessage) {
       console.log("Error", errorMessage);
@@ -136,6 +136,53 @@ var token= localStorage.getItem("token");
 console.log("token =>",token);
 
 var notes=[];
+
+$(function() {
+  $("#title_error_message").hide();
+  $("#desc_error_message").hide();
+  
+  var error_title = false;
+  var error_desc = false;
+  
+  
+  $("#title").focusout(function() {
+     check_title();
+  });
+  $("#desc").focusout(function() {
+     check_desc();
+  });
+  
+  
+  
+  function check_title() {
+    var pattern = /^[a-zA-Z ]*$/;
+    var title = $("#title").val()
+    if (pattern.test(title) && desc !== ''){
+         $("#title_error_message").hide();
+         $("#title").css("border-bottom","2px solid #34F458");
+      } else {
+         $("#title_error_message").html("Invalid title");
+         $("#title_error_message").show();
+         $("#title").css("border-bottom","2px solid #F90A0A");
+         error_title = true;
+      }
+   }
+  
+  function check_desc() {
+    var pattern = /^[a-zA-Z ]*$/;
+    var desc = $("#desc").val()
+    if (pattern.test(desc) && desc !== '') 
+    {
+      $("#desc_error_message").hide();
+      $("#title").css("border-bottom","2px solid #34F458");
+    } else {
+      $("#desc_error_message").html("Invalid description");
+      $("#desc_error_message").show();
+      $("#desc").css("border-bottom","2px solid #F90A0A");
+      error_desc = true;
+   }
+  }
+  
 $(document).ready(function(){
 
   $.ajax({
@@ -147,23 +194,169 @@ $(document).ready(function(){
       success: function(result){
       notes=result.data.data;
       console.log("notes = ",notes);
-// setTimeout(() => {
 
-      var wrapper = document.getElementById("display");
-      var myHTML = '';
+       var wrapper = document.getElementById("display");
+       var id = document.getElementById("id");
     
-      for (var i = 0; i <notes.length; i++) {
-        //myHTML += '<div class="card" data-toggle="modal" data-target="#myModal"><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span>'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div><div class="icons" style="padding-right: 10px;"><i class="material-icons">add_alert</i><i class="material-icons">person_add</i><i class="fas fa-palette"></i><i class="far fa-image"></i><img src="/images/archieve.png" style="width: 20px; height: 18px;"> <i class="fas fa-ellipsis-v"></i> </div></div></div></div>';
-        myHTML +='<div class="card" data-toggle="modal" data-target="#myModal"><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span>'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div><div class="icon dropdown" style="padding-right: 10px; display: flex;"><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="material-icons">add_alert</i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="material-icons">person_add</i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="fas fa-palette"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="far fa-image"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><img src="/images/archieve.png" style="width: 20px; height: 18px;"></i></div><div role="button" type="button" class="btn" data-toggle="dropdown"><i class="fas fa-ellipsis-v"></i></div><ul class="dropdown-menu" role="menu" aria-labelledby="menu1"><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Delete note</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Change labels</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Add drawing</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Make a copy</a></li><li role="presentation"><a role="menuitem" tabindex="-1" href="#">Show checkboxes</a></li></ul></div></div></div></div>';
+       var myHTML = ''; 
+       for (var i = 0; i <notes.length; i++) {  
+          myHTML +='<tr><div class="card" id='+[i]+'><div class="card-body"><div class="info" style="color: black; "><div class="tit" id="tit"><span class="tite">'+notes[i].title+'</span></div><div id="desc" class="desc" style="margin-top: 20px;"><span>'+notes[i].description+'</span></div></div><ul class="icon" style="padding-right: 10px; display: flex;"><li class="icon-item"><i class="material-icons" id="">add_alert</i></li><li class="icon-item"><i class="material-icons" id="">person_add</i></li>   <li class="icon-item"><i class="fas fa-palette" id=""></i></li><li class="icon-item"><i class="far fa-image" id=""></i></li><li class="icon-item" id="archieveNote"><img src="/images/archieve.png" style="width: 20px; height: 18px;"></i></li><li class="icon-item dropdown" ><i class="fas fa-ellipsis-v" role="button" data-toggle="dropdown" id="moreIcon"></i><ul class="dropdown-menu" aria-labelledby="moreIcon"><li class="dropdown-item" id="deleteNote">Delete note</li><li class="dropdown-item">Change labels</li><li class="dropdown-item">Add drawing</li><li class="dropdown-item">Make a copy</li></ul></li></ul></div></div></div></tr>';
       }
          wrapper.innerHTML = myHTML
-   // }, 1000);
-      
+       
    }});
 });
 
+
+/*********************** update note ************************************************************************/
 $(document).ready(function () {
-  $(".fas fa-ellipsis-v").click(function () {
-    $(".dropdown").toggle();
-  });
+  $(document).on("click" , ".info" , function(e) { 
+    // $('#myModal').toggle();
+    alert($(this).closest('div.card').attr('id'));
+    // console.log("data is ",('this').text());
+
+    // $tr = $(this).closest('tr').closest('.card').closest('.card-body').closest('.info');
+    // console.log($tr,"dom");
+
+    // var data =$tr.children("span").map(function(){
+    //       return $(this).text();
+    //     }).get();
+    //     console.log("data is :",data);
+    
+    //     $('.updateTitle').val(data[0]);
+    //     $('.updateDesc').val(data[1]);
+   });
 });
+
+
+$(document).ready(function(){
+  $("#updateData").click(function(){
+   alert("update  note");
+   console.log("update note");
+   
+    error_title = false;
+    error_desc = false;
+
+    check_title();
+    check_desc();
+
+   const updateNoteData={
+      title:$('.updateTitle').val(),
+      description:$('.updateTitle').val(),
+    };
+
+
+    title=$('.updateTitle').val();
+    description=$('.updateTitle').val();
+
+    console.log("title=",title);
+    console.log("desc=",description);
+    console.log(updateNoteData);
+
+
+if ( error_title === false && error_desc === false)
+{
+   var token= localStorage.getItem("token");
+   console.log("token =>",token);
+   console.log("notes data ",updateNoteData);
+   
+   $.ajax({
+    url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/updateNotes",
+    data:  JSON.stringify(updateNoteData),  
+    type: "POST",
+    headers: {
+       'Authorization': localStorage.getItem('token')
+       },
+    contentType: "application/json;charset=utf-8",
+    success: function (result) {
+    console.log("note update result ==>", result);
+
+     
+      console.log("id============>", result.id);
+      alert("note update Successfull");
+      resetForm();
+      $(location).attr('href',"/views/dashboard/dashboard.html");
+      },
+      error: function (errorMessage) {
+      console.log("Error", errorMessage);
+      alert("errorMessage==>",errorMessage.responseText);
+      }
+   })
+    return true;
+ } else {
+    alert("Please Fill Correctly");
+    return false;
+ }
+});
+});
+});
+
+// /********************* delete note **********************************************************/ 
+$(document).on("click" , "#deleteNote" , function(e) { 
+    
+    noteId=$(this).closest('div.card').attr('id');
+    alert("noteId = "+noteId); 
+   
+    data = notes[noteId].id;
+ 
+    $.ajax({
+      url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/trashNotes",
+      data:  JSON.stringify({
+        "isDeleted" : true, 
+        "noteIdList" : [data]
+      }),  
+      type: "POST",
+      headers: {
+         'Authorization': localStorage.getItem('token')
+       },
+      contentType: "application/json",
+      success: function (result) {
+      console.log("note delete result ==>", result);
+  
+       
+        console.log("id============>", result.id);
+        alert("note delete Successfull");
+        resetForm();
+        $(location).attr('href',"/views/dashboard/dashboard.html");
+        },
+        error: function (errorMessage) {
+        console.log("Error", errorMessage);
+        alert("errorMessage==>",errorMessage.responseText);
+        }
+     });
+  });
+  
+  /********************* archieve note **********************************************************/ 
+ 
+   $(document).on("click" , "#archieveNote" , function(e) { 
+    alert("in archieve");
+    noteId=$(this).closest('div.card').attr('id');
+        
+    data = notes[noteId].id;
+ 
+    $.ajax({
+      url: "http://fundoonotes.incubation.bridgelabz.com/api/notes/archiveNotes",
+      data:  JSON.stringify({
+        "isArchived" : true, 
+        "noteIdList" : [data]
+      }),   
+      type: "POST",
+      headers: {
+         'Authorization': localStorage.getItem('token')
+       },
+      contentType: "application/json;charset=utf-8",
+      success: function (result) {
+      console.log("note archive result ==>", result);
+  
+       
+        console.log("id============>", result.id);
+        alert("note archieve Successfull");
+        resetForm();
+        $(location).attr('href',"/views/dashboard/dashboard.html");
+        },
+        error: function (errorMessage) {
+        console.log("Error", errorMessage);
+        alert("errorMessage==>",errorMessage.responseText);
+        }
+     });
+  });
--- js/notes/note.js
+++ js/notes/note.js
@@ -1,8 +1,50 @@
-/**************************************************************************/
-$(document).ready(function () {
-    $(".notification_icon .fa-bell").click(function () {
-      $(".dropdown").toggle();
-    });
-  });
-  
- 
+console.log("hello in js");
+var innerHtml = "";
+var array=[
+    {
+        title:'test1',description:'description1',noteId:'sjhdhvsdv'
+    },
+    {
+        title:'test2',description:'description2',noteId:'jhdjhy'
+    },
+    {
+        title:'test3',description:'description3',noteId:'sdjhv'
+    },
+    {
+        title:'test4',description:'description4',noteId:'jhsvdyvsd'
+    },
+    {
+        title:'test5',description:'description5',noteId:'ysfduf'
+    }
+    
+    ]
+for (let i = 0; i < array.length; i++) {
+    innerHtml += '<div id='+i+' style="width: 200px;height: 60px;border: 1px solid gray;border-radius: 5px; padding: 10px;margin-bottom:4px;"> <div class="title" >'+array[i].title+'</div ><div class="des">'+array[i].description+'</div><img id="img" style="width: 25px;height: 25px;color: grey;" src="./assest/download.svg" alt=""><div id="menu" style="display:none; position: absolute;background: white;padding: 10px;box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302), 0 2px 6px 2px rgba(60,64,67,0.149);">delete</div></div>';
+}
+var container = document.getElementById('display');
+
+container.innerHTML=innerHtml;
+$(document).on("click" , "#display .title " , function(e) 
+{
+    console.log($(this).parent().attr("id"));
+    
+//    alert('hello world');
+});
+$(document).on("click" , "#img" , function(e) 
+{
+    console.log($(this).parent().attr("id"));
+    index=$(this).parent().attr("id");
+    var myitem = document.querySelectorAll("#menu");
+    console.log(myitem);
+    myitem[index].style.display = "block"
+//    alert('hello world');
+});
+$(document).on("click" , "#menu" , function(e) 
+{
+    console.log($(this).parent().attr("id"));
+    index=$(this).parent().attr("id");
+
+    console.log("id is ",array[index]);
+    
+//    alert('hello world');
+});
